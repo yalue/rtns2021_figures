0e58bc1d5aeb8bf7376a36e49a900184fa35e05e
Nathan O <Nathan O>
2020-11-16 13:06:25 -0500

    Update "worst-case" experiment
     - Updated the worst_case_experiment.py to the new planned format: 4 CDF
       plots showing 1024-vs-1024, 1024-vs-256, 256-vs-1024, and 256-vs-256.
    
     - Added placeholder data generated using the new script. About to
       replace with version generated when the GPU isn't also doing graphics.

--- worst_case_experiment/worst_case_experiment.py
+++ worst_case_experiment/worst_case_experiment.py
@@ -1,6 +1,7 @@
 # This script generates all of the data for the "worst practices" experiment
 # in the paper. Run it while in the `hip_plugin_framework` directory, then copy
 # the .json files from `hip_plugin_framework/results/`.
+import argparse
 import copy
 import json
 import subprocess
@@ -33,18 +34,12 @@ def generate_configs():
             "skip_copy": True
         }
     }
-
-    # Create a couple explicit "isolated" plugin configs in case we want to
-    # include "isolated" curves on later plots.
-    mm1024_isolated = copy.deepcopy(mm1024_config)
-    mm1024_isolated["label"] = "MM1024 (isolated)"
-    mm256_isolated = copy.deepcopy(mm256_config)
-    mm256_isolated["label"] = "MM256 (isolated)"
     to_return = []
+
     # We'll also copy and modify this top-level config, generally only to
     # change the scenario name.
     overall_config = {
-        "name": "MM1024 and MM256 Isolated",
+        "name": "MM1024 vs MM1024",
         "max_iterations": 0,
         "max_time": 10.0,
         "gpu_device_id": 0,
@@ -54,82 +49,122 @@ def generate_configs():
         "sync_every_iteration": False,
         "plugins": [mm1024_config]
     }
-    to_return.append(json.dumps(overall_config))
-    # Add the other plot of MM256 isolated to the scenario.
-    overall_config["plugins"] = [mm256_config]
-    to_return.append(json.dumps(overall_config))
 
-    # Now we'll make a plot of MM1024 and MM256, running at the same time.
-    overall_config["name"] = "MM1024 vs MM256, Full GPU Sharing"
-    mm1024_config["log_name"] = "./results/mm1024_full_shared.json"
-    mm256_config["log_name"] = "./results/mm256_full_shared.json"
-    overall_config["plugins"] = [mm1024_config, mm256_config]
-    to_return.append(json.dumps(overall_config))
-
-    # Now make MM1024 and MM256 run at the same time, but on partitions. We're
-    # "cheating" a little here for dramatic effect--we already know this is a
-    # good CU mask, but readers may not.
-    cu_mask_A = "10" * 30
-    cu_mask_B = "01" * 30
-    mm1024_config["compute_unit_mask"] = cu_mask_A
-    mm1024_config["label"] = "MM1024, 30 CU Partition"
-    mm1024_config["log_name"] = "./results/mm1024_equal_partitioned.json"
-    mm256_config["compute_unit_mask"] = cu_mask_B
-    mm256_config["label"] = "MM256, 30 CU Partition"
-    mm256_config["log_name"] = "./results/mm256_equal_partitioned.json"
-    overall_config["name"] = "MM1024 vs MM256, Evenly Partitioned Sharing"
-    overall_config["plugins"] = [mm1024_config, mm256_config]
-    to_return.append(json.dumps(overall_config))
-
-    # Now do the same thing, except change MM1024's CU mask to include one MORE
-    # CU--shared with MM256's partition.
-    cu_mask_A = "10" * 29 + "11"
-    mm1024_config["compute_unit_mask"] = cu_mask_A
-    mm1024_config["label"] = "MM1024, 31 CU Partition"
-    mm1024_config["log_name"] = "./results/mm1024_unequal_partitioned.json"
-    mm256_config["log_name"] = "./results/mm256_unequal_partitioned.json"
-    overall_config["name"] = "MM1024 vs MM256, Uneven Partitioned Sharing"
-    overall_config["plugins"] = [mm1024_config, mm256_config]
-    to_return.append(json.dumps(overall_config))
-
-    # Last plot, a thorough examination of MM1024 vs. itself in many different
-    # configurations. First, isolated:
-    mm1024_config = copy.deepcopy(mm1024_isolated)
+    # 1024: isolated
     mm1024_config["label"] = "Isolated"
-    mm1024_config["log_name"] = "./results/mm1024_isolated_exp2.json"
-    overall_config["name"] = "Various MM1024-only Configurations"
+    mm1024_config["log_name"] = "./results/1024_isolated.json"
+    overall_config["name"] = "MM1024 Isolated"
     overall_config["plugins"] = [mm1024_config]
     to_return.append(json.dumps(overall_config))
 
-    # Second, vs. an identical competitor on the full GPU:
-    mm1024_competitor = copy.deepcopy(mm1024_isolated)
-    mm1024_competitor["log_name"] = "/dev/null"
-    mm1024_competitor["label"] = "competitor"
-    mm1024_config["label"] = "Unpartitioned Sharing vs. Identical Competitor"
-    mm1024_config["log_name"] = "./results/mm1024_full_shared_exp2.json"
-    overall_config["plugins"] = [mm1024_config, mm1024_competitor]
+    # 256: isolated
+    mm256_config["label"] = "Isolated"
+    mm256_config["log_name"] = "./results/256_isolated.json"
+    overall_config["name"] = "MM256 Isolated"
+    overall_config["plugins"] = [mm256_config]
     to_return.append(json.dumps(overall_config))
 
-    # Third, vs. an identical competitor on equal partitions
-    mm1024_competitor["compute_unit_mask"] = "01" * 30
+    overall_config["name"] = "MM1024 vs MM1024"
+    # 1024 vs 1024: Fully shared GPU
+    mm1024_config["label"] = "Full GPU Sharing"
+    mm1024_config["log_name"] = "./results/1024_vs_1024_full_shared.json"
+    competitor1024 = copy.deepcopy(mm1024_config)
+    competitor1024["log_name"] = "/dev/null"
+    competitor1024["label"] = "Competitor"
+    overall_config["plugins"] = [mm1024_config, competitor1024]
+    to_return.append(json.dumps(overall_config))
+    # Partitioned equally
+    mm1024_config["label"] = "Evenly Partitioned"
+    mm1024_config["log_name"] = "./results/1024_vs_1024_evenly_partitioned.json"
     mm1024_config["compute_unit_mask"] = "10" * 30
-    mm1024_config["label"] = "Evenly Partitioned with Identical Competitor"
-    mm1024_config["log_name"] = "./results/mm1024_even_partitioned_shared_exp2.json"
-    overall_config["plugins"] = [mm1024_config, mm1024_competitor]
+    competitor1024["compute_unit_mask"] = "01" * 30
     to_return.append(json.dumps(overall_config))
-
-    # Third, vs. an identical competitor on unequal partitions
+    # Uneven partitioned
+    mm1024_config["label"] = "Partitioned, w/ Additional Shared CU"
+    mm1024_config["log_name"] = "./results/1024_vs_1024_unevenly_partioned.json"
     mm1024_config["compute_unit_mask"] = "10" * 29 + "11"
-    mm1024_config["label"] = "Unevenly Partitioned with Identical Competitor"
-    mm1024_config["log_name"] = "./results/mm1024_uneven_partitioned_shared_exp2.json"
-    overall_config["plugins"] = [mm1024_config, mm1024_competitor]
     to_return.append(json.dumps(overall_config))
 
+    # Remove the CU masks
+    del mm1024_config["compute_unit_mask"]
+    del competitor1024["compute_unit_mask"]
+
+    overall_config["name"] = "MM1024 vs MM256"
+    # 1024 vs 256: Fully shared GPU
+    mm1024_config["log_name"] = "./results/1024_vs_256_full_shared.json"
+    mm1024_config["label"] = "Full GPU Sharing"
+    competitor256 = copy.deepcopy(mm256_config)
+    competitor256["log_name"] = "/dev/null"
+    competitor256["label"] = "Competitor"
+    overall_config["plugins"] = [mm1024_config, competitor256]
+    to_return.append(json.dumps(overall_config))
+    # Partitioned equally
+    mm1024_config["log_name"] = "./results/1024_vs_256_evenly_partitioned.json"
+    mm1024_config["label"] = "Evenly Partitioned"
+    mm1024_config["compute_unit_mask"] = "10" * 30
+    competitor256["compute_unit_mask"] = "01" * 30
+    to_return.append(json.dumps(overall_config))
+    # Uneven partitioned
+    mm1024_config["log_name"] = "./results/1024_vs_256_unevenly_partitioned.json"
+    mm1024_config["label"] = "Partitioned, w/ Additional Shared CU"
+    mm1024_config["compute_unit_mask"] = "10" * 29 + "11"
+    to_return.append(json.dumps(overall_config))
+
+    # Remove CU masks again
+    del mm1024_config["compute_unit_mask"]
+    del competitor256["compute_unit_mask"]
+
+    overall_config["name"] = "MM256 vs MM256"
+    # 256 vs 256: Fully shared GPU
+    mm256_config["log_name"] = "./results/256_vs_256_full_shared.json"
+    mm256_config["label"] = "Full GPU Sharing"
+    overall_config["plugins"] = [mm256_config, competitor256]
+    to_return.append(json.dumps(overall_config))
+    # Partitioned equally
+    mm256_config["log_name"] = "./results/256_vs_256_evenly_partitioned.json"
+    mm256_config["label"] = "Evenly Partitioned"
+    mm256_config["compute_unit_mask"] = "10" * 30
+    competitor256["compute_unit_mask"] = "01" * 30
+    to_return.append(json.dumps(overall_config))
+    # Uneven partitioned
+    mm256_config["log_name"] = "./results/256_vs_256_unevenly_partitioned.json"
+    mm256_config["label"] = "Partitioned, w/ Additional Shared CU"
+    mm256_config["compute_unit_mask"] = "10" * 29 + "11"
+    to_return.append(json.dumps(overall_config))
+
+    # Remove CU masks again
+    del mm256_config["compute_unit_mask"]
+    del competitor256["compute_unit_mask"]
+
+    overall_config["name"] = "MM256 vs MM1024"
+    # 256 vs 1024: Fully shared GPU
+    mm256_config["log_name"] = "./results/256_vs_1024_full_shared.json"
+    mm256_config["label"] = "Full GPU Sharing"
+    overall_config["plugins"] = [mm256_config, competitor1024]
+    to_return.append(json.dumps(overall_config))
+    # Partitioned equally
+    mm256_config["log_name"] = "./results/256_vs_1024_evenly_partitioned.json"
+    mm256_config["label"] = "Evenly Partitioned"
+    mm256_config["compute_unit_mask"] = "10" * 30
+    competitor1024["compute_unit_mask"] = "01" * 30
+    to_return.append(json.dumps(overall_config))
+    # Uneven partitioned
+    mm256_config["log_name"] = "./results/256_vs_1024_unevenly_partitioned.json"
+    mm256_config["label"] = "Partitioned, w/ Additional Shared CU"
+    mm256_config["compute_unit_mask"] = "10" * 29 + "11"
+    to_return.append(json.dumps(overall_config))
+
+    # All done
     return to_return
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start_experiment", type=int, default=1,
+        help="The number of the first experiment to resume from, for use if "+
+            "one of the tests hangs.")
+    args = parser.parse_args()
     configs = generate_configs()
-    for i in range(len(configs)):
+    for i in range(args.start_experiment - 1, len(configs)):
         print("Running experiment %d of %d" % (i + 1, len(configs)))
         config = configs[i]
         process = subprocess.Popen(["./bin/runner", "-"],
